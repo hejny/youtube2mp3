b1777041de69e0d580f226f891bb7e0146835357
Pavol Hejný <me@pavolhejny.com>
2016-05-15 03:56:46 +0200

    first commit

---
+++ app/api/item.php
@@ -0,0 +1,94 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+$method = strtoupper($_SERVER['REQUEST_METHOD']);
+
+
+
+$filename = __DIR__ . '/../../download/list.json';
+
+
+
+
+if($method == 'OPTIONS'){
+
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+
+}elseif($method=='GET'){
+
+    header('Content-Type: application/json');
+    if (file_exists($filename)) {
+        readfile($filename);
+    }else{
+        echo('[]');
+    }
+
+}elseif($method=='POST') {
+
+
+    if (file_exists($filename)) {
+        $file = file_get_contents($filename);
+        $json = json_decode($file, true);
+    }else{
+        $json=array();
+    }
+
+    $inlist=false;
+    foreach($json as $video){
+        if($video['url']==$_POST['url']){
+            $inlist=true;
+        }
+    }
+
+    if(!$inlist){
+
+        $json[]=array(
+            "url"=>$_POST['url'],
+            "actions"=>array(
+                "added"=>time(),
+                "downloading"=>array("start"=>false,"finish"=>false)
+            ),
+            "errors"=> []
+        );
+
+    }else{
+
+        $json=array_filter($json,function($video){
+
+            if($video['url']==$_POST['url'] && $video['actions']['downloading']['start']===false){
+                return false;
+            }else{
+                return true;
+            }
+
+        });
+
+    }
+
+
+    $file=json_encode($json, JSON_PRETTY_PRINT);
+
+    file_put_contents($filename,$file);
+    chmod($filename,0777);
+
+
+}else{
+
+    http_response_code(400);//Bad Request
+
+}
+
+
+
+
+
+
+
+
+
---
+++ app/browser.js
@@ -0,0 +1,131 @@
+(function() {
+
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+
+    var checkReady = function(callback) {
+        if (window.jQuery) {
+            callback(jQuery);
+        }
+        else {
+            window.setTimeout(function() { checkReady(callback); }, 100);
+        }
+    };
+
+
+    checkReady(function($) {
+
+
+        var reload = function() {
+            var request = $.get('https://localhost/youtube/app/api/item.php').done(function (response) {
+
+                var response = Object.keys(response).map(function(k) { return response[k] });
+                console.log(response);
+
+
+                $('.addbutton').remove();
+
+                var links = $('.video-list').find('a[href^="/watch"]');
+
+
+
+                links.each(function () {
+
+
+                    var plus;
+                    plus = $('<div>+</div>');
+
+
+                    plus.addClass('addbutton');
+                    plus.css('float', 'right');
+                    plus.css('width', '50px');
+                    plus.css('height', '50px');
+                    plus.css('border', '2px solid #00f');
+                    plus.css('border-radius', '500px');
+                    plus.css('background', '#fff');
+                    plus.css('z-index', 99999);
+
+                    plus.css('text-align', 'center');
+                    plus.css('font-size', '40px');
+                    plus.css('color', '#00f');
+                    plus.attr('inlist', 'no');
+
+
+                    var url = $(this)[0].href;
+
+
+
+                    response.forEach(function (video) {
+                        if (video.url === url) {
+
+
+                            if(video.actions.downloading.finish!==false){
+                                plus.text('✓');
+                                plus.css('background', '#6f9');
+                            }else /**/
+                            if(video.actions.downloading.start!==false){
+                                plus.text('✓');
+                                plus.css('background', '#ff5');
+                            }else{
+                                plus.text('✓');
+                                plus.css('background', '#ccc');
+                            }
+
+
+                            plus.attr('inlist', 'yes');
+
+                        }
+                    });
+
+
+                    plus.click(function (e) {
+
+                        e.preventDefault();
+
+                        var button = $(this);
+                        button.text('');
+                        button.css('background', '#eee');
+
+
+                        var request = $.post(
+                            'https://localhost/youtube/app/api/item.php', {url: url}
+                        ).done(
+                            function () {
+
+                                /*if (button.attr('inlist') == 'no') {
+                                    button.attr('inlist', 'yes');
+                                    button.css('background', '#6f9');
+                                } else {
+                                    button.attr('inlist', 'no');
+                                    button.css('background', '#fff');
+                                }*/
+                                reload();
+
+                            }
+                        ).fail(
+                            function () {
+
+                                reload();
+                                /*button.css('background', '#f00');*/
+                            }
+                        );
+
+
+                    });
+
+                    $(this).append(plus);
+
+
+                });
+            });
+
+        };
+
+        reload();
+
+
+    });
+})();
